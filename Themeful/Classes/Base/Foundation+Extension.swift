81bc35620e23637961a1462625e02efe1a396130
XMFraker <3057600441@qq.com>
2019-01-14 17:15:56 +0800

    move func of UIKit & Foundation

--- Themeful/Classes/Base/Foundation+Extension.swift
+++ Themeful/Classes/Base/Foundation+Extension.swift
@@ -8,3 +8,57 @@
 //  @abstract   <#class description#>
 
 import Foundation
+
+internal extension FileManager {
+    
+    internal func fileExists(at URL: URL, isDirectory: UnsafeMutablePointer<ObjCBool>? = nil) -> Bool {
+        if URL.isFileURL { return fileExists(atPath: URL.path, isDirectory: isDirectory) }
+        else { return fileExists(atPath: URL.absoluteString, isDirectory: isDirectory) }
+    }
+    
+    internal func renameFile(at URL: URL, with name: String, overwrite: Bool = false) -> Bool {
+        
+        // file doesnot exists
+        guard fileExists(at: URL) else { return false }
+        
+        let dstURL = URL.deletingLastPathComponent().appendingPathComponent(name)
+        
+        // remove exists dst if need overwrite
+        if fileExists(at: URL) {
+            if overwrite == false { return true }
+            else { try? removeItem(at: dstURL) }
+        }
+        
+        if let _ = try? moveItem(at: URL, to: dstURL) { return true }
+        return false
+    }
+}
+
+internal extension URL {
+    
+    internal var isJSONFile: Bool {
+        return self.pathExtension == PlistFileExtension || self.pathExtension == JSONFileExtension
+    }
+    
+    internal func JSON() -> NSDictionary? {
+        
+        if self.pathExtension == PlistFileExtension, let info = NSDictionary(contentsOf: self) {
+            return info
+        }
+        
+        if let data = NSData(contentsOf: self) {
+            let json = try? JSONSerialization.jsonObject(with: data as Data, options: .mutableContainers) as? NSDictionary
+            return json ?? nil
+        }
+        
+        return nil
+    }
+}
+
+internal extension Array where Element: Hashable {
+    
+    internal func filterDuplicate() -> [Element] {
+        if self.isEmpty { return [] }
+        return Array(Set(self))
+    }
+}
