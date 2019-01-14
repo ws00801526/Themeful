5fce50258a075ae61543cef12e5b28015ac37bbd
XMFraker <3057600441@qq.com>
2019-01-14 17:15:35 +0800

    add Extension file of Foundation & UIKit

---
+++ Themeful/Classes/Base/Foundation+Extension.swift
@@ -0,0 +1,10 @@
+//  Foundation+Extension.swift
+//  Pods
+//
+//  Created by  XMFraker on 2019/1/14
+//  Copyright © XMFraker All rights reserved. (https://github.com/ws00801526)
+//  @class      Foundation_Extension
+//  @version    <#class version#>
+//  @abstract   <#class description#>
+
+import Foundation
---
+++ Themeful/Classes/Base/UIKit+Extension.swift
@@ -0,0 +1,10 @@
+//  UIKit+Extension.swift
+//  Pods
+//
+//  Created by  XMFraker on 2019/1/14
+//  Copyright © XMFraker All rights reserved. (https://github.com/ws00801526)
+//  @class      UIKit_Extension
+//  @version    <#class version#>
+//  @abstract   <#class description#>
+
+import Foundation
